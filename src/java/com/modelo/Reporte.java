e9d03f2c2598cfa6b9386c4ef069b0cf3a26c934
IrminDev <ihernandezj1902@alumno.ipn.mx>
2022-04-14 14:12:22 -0500

    initial commit

---
+++ src/java/com/modelo/Reporte.java
@@ -0,0 +1,116 @@
+package com.modelo;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author IrminDev
+ */
+public class Reporte {
+    private int idReporte;
+    private String descripcion;
+    private int idTipoReporte;
+    private String tipoReporte;
+    private Date inicio;
+    private Date fin;
+    private int idEncargado;
+    private String nombreEncargado;
+    private int idEstatus;
+    private String estatus;
+
+    public int getIdReporte() {
+        return idReporte;
+    }
+
+    public void setIdReporte(int idReporte) {
+        this.idReporte = idReporte;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getIdTipoReporte() {
+        return idTipoReporte;
+    }
+
+    public void setIdTipoReporte(int idTipoReporte) {
+        this.idTipoReporte = idTipoReporte;
+        switch(idTipoReporte){
+            case(1):
+                this.tipoReporte = "Soporte";
+                break;
+            case(2):
+                this.tipoReporte = "Mantenimiento";
+            default:
+                break;
+        }
+    }
+
+    public Date getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Date inicio) {
+        this.inicio = inicio;
+    }
+
+    public Date getFin() {
+        return fin;
+    }
+
+    public void setFin(Date fin) {
+        this.fin = fin;
+    }
+
+    public int getIdEncargado() {
+        return idEncargado;
+    }
+
+    public void setIdEncargado(int idEncargado) {
+        this.idEncargado = idEncargado;
+    }
+
+    public String getNombreEncargado() {
+        return nombreEncargado;
+    }
+
+    public void setNombreEncargado(String nombreEncargado) {
+        this.nombreEncargado = nombreEncargado;
+    }
+
+    public int getIdEstatus() {
+        return idEstatus;
+    }
+
+    public void setIdEstatus(int idEstatus) {
+        this.idEstatus = idEstatus;
+        switch(idEstatus){
+            case(1):
+                this.estatus = "Abierto";
+                break;
+            case(2):
+                this.estatus = "En proceso";
+            case(3):
+                this.estatus = "En mantenimiento";
+            case(4):
+                this.estatus = "Cerrado";
+            default:
+                break;
+        }
+    }
+
+    public String getTipoReporte() {
+        return tipoReporte;
+    }
+    
+    public String getEstatus() {
+        return estatus;
+    }
+
+    
+}
---
+++ src/java/com/modelo/Usuario.java
@@ -0,0 +1,55 @@
+package com.modelo;
+
+/**
+ *
+ * @author IrminDev
+ */
+public class Usuario {
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasena;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    
+}
